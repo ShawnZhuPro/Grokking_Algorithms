108afe7e980cd4bc60b0c8740468bb74ec0b1b1d
ShawnZhuPro <shawnz05827@gmail.com>
2023-08-16 09:06:58 -0700

    Create Binary_Search.py

---
+++ Binary_Search.py
@@ -0,0 +1,22 @@
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        
+        if guess == item:  # If the guessed value is equal to the item we're searching for
+            return mid  # Return the index where the item is found
+        
+        if guess > item:  # If the guessed value is greater than the item
+            high = mid - 1  # Adjust the upper bound to search in the lower half
+        else:
+            low = mid + 1  # Adjust the lower bound to search in the upper half
+        
+    return None  # Return None if the item is not found in the list
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 3)) # Output should be the index of 1
+print(binary_search(my_list, 0)) # Output should be None
